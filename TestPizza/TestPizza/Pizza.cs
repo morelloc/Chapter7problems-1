bcb794aeb6b1a62b14a6a1d3a5c3280f524b8cc7
BicBoi <austinmatase@gmail.com>
2013-09-12 08:10:22 -0400

    Number 1 Done

---
+++ TestPizza/TestPizza/Pizza.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter7Problems
+{
+    class Pizza
+    {
+        private string toppings;
+        private double price;
+        private int diameter;
+        public string Toppings
+        {
+            get
+            {
+                return toppings;
+            }
+            set
+            {
+                toppings = value;
+            }
+        }
+
+        public int Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                diameter = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
---
+++ TestPizza/TestPizza/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter7Problems
+{
+    class TestPizza
+    {
+        public static void Main()
+        {
+            Pizza pizza = new Pizza();
+            pizza.Price = 10.00;
+            pizza.Diameter = 12;
+            pizza.Toppings = "Pepperoni";
+        }
+    }
+}
