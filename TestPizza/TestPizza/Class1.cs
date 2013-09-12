de1e9493a2450a1ca7cb92865ede02b0fb16ea56
BicBoi <austinmatase@gmail.com>
2013-09-12 08:36:42 -0400

    Started GirlScout

---
+++ TestPizza/TestPizza/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            Girlscout name = new Girlscout();
+            Console.WriteLine("to obey the Girl Scout law");
+
+
+    }
+}
---
+++ TestPizza/WindowsFormsApplication1/Girlscout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Girlscout
+    {
+        private string name;
+        private double troop;
+        private double dues;
+        private string motto;
+    }
+}
